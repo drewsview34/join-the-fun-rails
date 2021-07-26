ca9a1616340d4d24e112b0f003b7f0f176a67c35
drewsview34 <drewsview34@outlook.com>
2021-07-26 11:15:32 -0700

    deleted join table and added foreign key to rides in passenger and taxi tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210726175616) do
+ActiveRecord::Schema.define(version: 20210726181215) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20210726175616) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
